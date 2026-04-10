0295f51c30babc56bc62a2d604903a50036bc398
23wh1a0510 <23wh1a0510@bvrithyderabad.edu.in>
2026-04-10 13:23:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ function App() {
   return (
     <div>
       <Home />
+      <h2>Task data</h2>
+      <input type="text" placeholder="Enter task data" />
+      <button>Add task</button>
+      <ul>
+        <li>Task 1</li>
+        <li>Task 2</li>
+      </ul>
+      <input type = "file" placeholder="Upload file" />
+      <button>Upload</button>
+      <p>file uploaded successfully</p>
+      "file" value = {"fileee.txt"} onChange = {handleFileChange}
+      
+      
 
-      {/* TODO: Replace this placeholder with Routes */}
     </div>
   );
 }
